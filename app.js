8400fe8ed6c075aeaf88e15a6058258b3cb9fe55
Dan Torczynski <dtorczynski@gmail.com>
2015-08-16 15:28:55 -0700

    Hello world with mongo

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
-
 var app = express();
-
 var bodyParser = require('body-parser');
 var jsonencode = bodyParser.json();
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/SWIPE_SECRETS';
+
 app.use(express.static('app'));
+
+// perhaps move all this init param stuff to separate script to only run once.
 
 
 app.get('/init', function(request,response){
@@ -25,5 +29,17 @@ app.post('/new_choice', jsonencode, function(request,response){
     response.sendStatus(201);
 });
 // when session closes, if fewer than n entries, perhaps delete session
+
+
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+
+
+
 module.exports = app;
 
